ae11d3fd5941be9fda75cfd08aecabe9c6be1334
Bohdan Shtepan <winsatana@ya.ru>
2016-05-28 05:52:09 +0300

    Adds RippleWave class

---
+++ src/components/ripple/RippleWave.js
@@ -0,0 +1,414 @@
+/**
+ * This file\code is part of Paper UI project.
+ *
+ * Paper UI - is a modern front-end framework based on Material Design by Google
+ * https://github.com/paper-ui
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the MIT license.
+ */
+
+import { ce, now } from '../../core/utils';
+import ElementRect from '../../core/ElementRect';
+
+/**
+ * Provides all the logic to produce a one-time rippling effect.
+ *
+ * @class PaperWave
+ */
+export default class RippleWave {
+    // <editor-fold desc="Class Constructor">
+
+    /**
+     * Initializes a new instance of the `PaperWave` class with the specified `PaperRipple` instance.
+     */
+    constructor({ $, recenters = false, center = false, initialOpacity = 0.25, opacityDecayVelocity = 0.8 }) {
+        if (!$) {
+            throw new ReferenceError('$ is null');
+        }
+
+        /**
+         * Gets or sets the color of the wave.
+         *
+         * @type {String}
+         */
+        this.color = window.getComputedStyle($).color;
+
+        /**
+         * Gets or sets the container metrics of the wave.
+         *
+         * @type {ElementRect}
+         */
+        this.containerRect = new ElementRect($);
+
+        /**
+         * Determines whether the wave should be re-centered towards the center of its container.
+         *
+         * @type {Boolean}
+         */
+        this.recenters = recenters;
+
+        /**
+         * Determines whether the wave should start a movement from the center of its container.
+         *
+         * @type {Boolean}
+         */
+        this.center = center;
+
+        /**
+         * Gets initial opacity of the wave.
+         *
+         * @type {Number}
+         */
+        this.initialOpacity = initialOpacity;
+
+        /**
+         * Gets opacity decay velocity of the wave.
+         *
+         * @type {Number}
+         */
+        this.opacityDecayVelocity = opacityDecayVelocity;
+
+        /**
+         * Represents the object wrapped around the `wave` DOM element that belongs to the current instance.
+         *
+         * @type {Object}
+         */
+        this.$wave = ce('div');
+
+        this.$wave.classList.add('paper-ripple__wave');
+        this.$wave.style.backgroundColor = this.color;
+
+        /**
+         * Represents the object wrapped around the main DOM element that belongs to the current instance.
+         *
+         * @type {Object}
+         */
+        this.$ = ce('div');
+
+        this.$.classList.add('paper-ripple__wave-container');
+        this.$.appendChild(this.$wave);
+
+        this.resetDefaults();
+
+        return this;
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="Class Props">
+
+    /**
+     * Represents the max possible value of the wave's radius.
+     *
+     * @const {Number}
+     * @default
+     */
+    static MAX_RADIUS = 300;
+
+    /**
+     * Gets the time in milliseconds elapsed from the moment where interaction with the wave was started.
+     *
+     * @returns {Number} The time in milliseconds.
+     */
+    get touchDownElapsed() {
+        let elapsed;
+
+        if (!this.touchDownStarted) {
+            return 0;
+        }
+
+        elapsed = now() - this.touchDownStarted;
+
+        if (this.touchUpStarted) {
+            elapsed -= this.touchUpElapsed;
+        }
+
+        return elapsed;
+    }
+
+    /**
+     * Gets the time in milliseconds elapsed from the moment where interaction with the wave was ended.
+     *
+     * @returns {Number} The time in milliseconds.
+     */
+    get touchUpElapsed() {
+        return this.touchUpStarted ? now() - this.touchUpStarted : 0;
+    }
+
+    /**
+     * Gets the time in seconds elapsed since the moment where interaction with the wave was started.
+     *
+     * @returns {Number} The time in seconds.
+     */
+    get touchDownElapsedSeconds() {
+        return this.touchDownElapsed / 1000;
+    }
+
+    /**
+     * Gets the time in seconds elapsed since the moment where interaction with the wave was ended.
+     *
+     * @returns {number} The time in seconds.
+     */
+    get touchUpElapsedSeconds() {
+        return this.touchUpElapsed / 1000;
+    }
+
+    /**
+     * Gets the total interaction time.
+     *
+     * @returns {Number} The time in seconds
+     */
+    get mouseInteractionSeconds() {
+        return this.touchDownElapsedSeconds + this.touchUpElapsedSeconds;
+    }
+
+    /**
+     * Gets the wave's radius at the current time.
+     *
+     * @returns {Number} The value of the wave's radius.
+     */
+    get radius() {
+        const radius = Math.min(
+                Math.sqrt(Math.pow(this.containerRect.width, 2) + Math.pow(this.containerRect.height, 2)),
+                RippleWave.MAX_RADIUS
+                ) * 1.1 + 5,
+            elapsed = 1.1 - 0.2 * (radius / RippleWave.MAX_RADIUS),
+            currentTime = this.mouseInteractionSeconds / elapsed,
+            actualRadius = radius * (1 - Math.pow(80, -currentTime));
+
+        return Math.abs(actualRadius);
+    }
+
+    /**
+     * Gets the wave's opacity at the current time.
+     *
+     * @returns {Number} The value of the wave's opacity.
+     */
+    get opacity() {
+        if (!this.touchUpStarted) {
+            return this.initialOpacity;
+        }
+
+        return Math.max(0, this.initialOpacity - this.touchUpElapsedSeconds * this.opacityDecayVelocity);
+    }
+
+    /**
+     * Gets the wave's outer opacity at the current time.
+     *
+     * @returns {Number} The value of the wave's outer opacity.
+     */
+    get outerOpacity() {
+        return Math.max(0, Math.min(this.touchUpElapsedSeconds * 0.3, this.opacity));
+    }
+
+    /**
+     * Determines whether the wave is fully opaque or not.
+     *
+     * @returns {Boolean} `true`, if so, otherwise `false`.
+     */
+    get isWaveFullyOpaque() {
+        return this.opacity < 0.01 && this.radius >= Math.min(this.maxRadius, RippleWave.MAX_RADIUS);
+    }
+
+    /**
+     * Determines whether the wave reached its max radius or not.
+     *
+     * @returns {Boolean} `true`, if so, otherwise `false`.
+     */
+    get isMaxRadiusReached() {
+        return this.opacity >= this.initialOpacity && this.radius >= Math.min(this.maxRadius, RippleWave.MAX_RADIUS);
+    }
+
+    /**
+     * Determines whether the animation of rippling effect completed or not.
+     *
+     * @returns {Boolean} `true`, if so, otherwise `false`.
+     */
+    get isAnimationComplete() {
+        return this.touchUpStarted ? this.isWaveFullyOpaque : this.isMaxRadiusReached;
+    }
+
+    /**
+     * Gets the wave's translation fraction value.
+     *
+     * @returns {Number} The value of the wave's translation fraction.
+     */
+    get translationFraction() {
+        return Math.min(1, this.radius / this.containerRect.size * 2 / Math.sqrt(2));
+    }
+
+    /**
+     * Gets the wave's current position.
+     *
+     * @returns {{x: Number, y: Number}} Object containing coordinates of the wave's current position.
+     */
+    get currentPosition() {
+        const translateFraction = this.translationFraction;
+        let x = this.startPosition.x,
+            y = this.startPosition.y;
+
+        if (this.endPosition.x) {
+            x = this.startPosition.x + translateFraction * (this.endPosition.x - this.startPosition.x);
+        }
+
+        if (this.endPosition.y) {
+            y = this.startPosition.y + translateFraction * (this.endPosition.y - this.startPosition.y);
+        }
+
+        return { x, y };
+    }
+
+    /**
+     * Determines whether the pointing device is still in interaction with the current wave.
+     *
+     * @returns {Boolean} `true`, if so, otherwise `false`.
+     */
+    get isTouchDown() {
+        return this.touchDownStarted && !this.touchUpStarted;
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="Class Methods">
+
+    /**
+     * Resets all the wave's values.
+     *
+     * @returns {RippleWave} Current instance for method chaining.
+     */
+    resetDefaults() {
+        /**
+         * Gets or sets max radius of the wave.
+         *
+         * @type {Number}
+         */
+        this.maxRadius = 0;
+
+        /**
+         * Gets or sets the time of starting interaction with the wave.
+         *
+         * @type {Number}
+         */
+        this.touchDownStarted = 0;
+
+        /**
+         * Gets or sets the time of ending interaction with the wave.
+         *
+         * @type {Number}
+         */
+        this.touchUpStarted = 0;
+
+        /**
+         * Gets or sets the start position of the wave.
+         *
+         * @type {{x: Number, y: Number}}
+         */
+        this.startPosition = { x: 0, y: 0 };
+
+        /**
+         * Gets or sets the end position of the wave.
+         *
+         * @type {{x: Number, y: Number}}
+         */
+        this.endPosition = { x: 0, y: 0 };
+
+        return this;
+    }
+
+    /**
+     * Performs updating of the wave's values.
+     *
+     * @returns {RippleWave} Current instance for method chaining.
+     */
+    draw() {
+        let cssString;
+        const scaleFactor = this.radius / (this.containerRect.size / 2),
+            containerCenter = this.containerRect.center,
+            currentPos = this.currentPosition,
+            deltaPos = {
+                x: currentPos.x - containerCenter.x,
+                y: currentPos.y - containerCenter.y
+            };
+
+        this.$wave.style.opacity = this.opacity;
+
+        cssString = `translate(${deltaPos.x}px, ${deltaPos.y}px)`;
+        this.$.style.webkitTransform = cssString;
+        this.$.style.mozTransform = cssString;
+        this.$.style.msTransform = cssString;
+        this.$.style.oTransform = cssString;
+        this.$.style.transform = `translate3d(${deltaPos.x}px, ${deltaPos.y}px, 0)`;
+
+        cssString = `scale(${scaleFactor}, ${scaleFactor})`;
+        this.$wave.style.webkitTransform = cssString;
+        this.$wave.style.mozTransform = cssString;
+        this.$wave.style.msTransform = cssString;
+        this.$wave.style.oTransform = cssString;
+        this.$wave.style.transform = `scale3d(${scaleFactor}, ${scaleFactor}, 1)`;
+
+        return this;
+    }
+
+    /**
+     * Performs ripple-down effect on the current wave.
+     *
+     * @param {(Event|Object)} [event=null] - An object containing coordinates of interaction point to set start position of ripple effect.
+     * @returns {RippleWave} Current instance for method chaining.
+     */
+    downAction(event = null) {
+        const containerCenter = this.containerRect.center;
+
+        this.resetDefaults();
+
+        this.touchDownStarted = now();
+        this.startPosition = this.center || !event ?
+            containerCenter :
+            {
+                x: (event.clientX || event.x) - this.containerRect.boundingRect.left,
+                y: (event.clientY || event.y) - this.containerRect.boundingRect.top
+            };
+        this.endPosition = this.recenters ? containerCenter : this.endPosition;
+        this.maxRadius = this.containerRect.distanceToFarthestCorner(this.startPosition);
+
+        this.$.style.top = `${(this.containerRect.height - this.containerRect.size) / 2}px`;
+        this.$.style.left = `${(this.containerRect.width - this.containerRect.size) / 2}px`;
+        this.$.style.width = `${this.containerRect.size}px`;
+        this.$.style.height = `$this.containerRect.size}px`;
+
+        return this;
+    }
+
+    /**
+     * Performs ripple-up effect on the current wave.
+     *
+     * @returns {RippleWave} Current instance for method chaining.
+     */
+    upAction() {
+        if (!this.isTouchDown) {
+            return this;
+        }
+
+        /**
+         * @private
+         */
+        this.touchUpStarted = now();
+
+        return this;
+    }
+
+    /**
+     * Removes the wave from a DOM.
+     *
+     * @returns {RippleWave} Current instance for method chaining.
+     */
+    remove() {
+        this.$.parentNode.removeChild(this.$);
+
+        return this;
+    }
+
+    // </editor-fold>
+}
